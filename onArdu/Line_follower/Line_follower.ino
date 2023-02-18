6f214c6a6ba7a980a4a96a12e36e7aca118bd75e
zebra314 <kidalfonso@gmail.com>
2023-02-18 12:21:30 +0800

    Add communication format

---
+++ onArdu/Line_follower/Line_follower.ino
@@ -0,0 +1,20 @@
+void setup() {
+    Serial.begin(57600);
+}
+
+/**************************************
+The message received from the Pi will be in string type. 
+Normally, the data will be numbers with two digits, representing the percentage of the offset from the mid point.
+For example, "10" means the current direction has 10 percent offset from the mid point at the moment.
+When the process is finished, Arduino will receive "stop" message.
+***************************************/ 
+
+String message;
+void loop() {
+
+    // if there are msgs available
+    if (Serial.available() > 0){
+      message = Serial.readString();
+      Serial.println(message);
+    }
+}
