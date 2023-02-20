24f913caf114a822b6cfd61dd63e54e3679093f1
alfonso314159 <kidalfonso@gmail.com>
2023-02-20 11:53:00 +0800

    Update img_process

--- onPi/src/detector/src/Line_detector.py
+++ onPi/src/detector/src/Line_detector.py
@@ -18,7 +18,9 @@ class Line_detector:
         self.bridge = CvBridge()
         self.recorder = Recorder('line_detector')
         self.pub = rospy.Publisher('/offset', String, queue_size = 1)
-        self.offset = 0
+        self.offset_1 = 0
+        self.offset_2 = 0
+        self.offset_3 = 0
 
         rospy.init_node('line_detector')
         rate = rospy.Rate(10)
@@ -35,7 +37,6 @@ class Line_detector:
         """
         Execute when receiving the msg from camera topic
         """
-        frame = self.bridge.imgmsg_to_cv2(frame, 'bgr8') 
         frame = self.img_process(frame)
         frame = self.plot_position(frame)
         self.recorder(frame)
@@ -52,7 +53,9 @@ class Line_detector:
         Execute when receiving the msg from endgame topic.
         Using the self.going to break the loop in __call__.
         """
-        self.going = data.data 
+        if data.data == 'stop':
+            rospy.signal_shutdown('line_detector terminate')
+ 
             
     def signal_handler(self):
         self.recorder.writer.release()
@@ -60,7 +63,11 @@ class Line_detector:
         print('\nline_detector stop\n')
 
     def img_process(self, frame):
+        frame = self.bridge.imgmsg_to_cv2(frame, 'bgr8')
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # 反轉 
+        frame = cv2.bitwise_not(frame)
         
         # 模糊處理
         frame = cv2.GaussianBlur(frame, (7, 7), 0) 
@@ -75,21 +82,47 @@ class Line_detector:
         return frame
     
     def plot_position(self, frame):
-        # 反轉 
-        frame_inv = cv2.bitwise_not(frame)
-        
-        M = cv2.moments(frame_inv)
+        # 將畫面分為兩部分
+        # 上分三之一 中間三分之一
+
+        H, W, _ = frame.shape
+
+        # crop
+        frame_top = frame[0:int(H/3), 0:W]
+        frame_mid = frame[int(H/3):H, 0:W]
+
+        # moment
+        M_top = cv2.moments(frame_top)
+        M_mid = cv2.moments(frame_mid)
+        M = cv2.moments(frame)
 
         frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-        H, W, _ = frame.shape
+        
+        # center line
         frame = cv2.line(frame,(int(W/2),0),(int(W/2),int(H)),(55,255,255),2)
 
-        if M["m00"] != 0:
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-    
-        frame = cv2.circle(frame, (int(cX), int(H/2)), 5, (0, 0, 255), -1)
-        frame = cv2.line(frame,(int(cX),0),(int(cX),int(H*2)),(0, 0, 255),2)
+        # moment calculate using top section of the frame
+        if M_top["m00"] != 0:
+            cX_top = int(M["m10"] / M["m00"])
+            # cY_top = int(M["m01"] / M["m00"])
+        frame = cv2.circle(frame, (int(cX_top), int(H/6)), 6, (0, 0, 255), -1)
+        frame = cv2.line(frame,(int(cX_top),0),(int(cX_top),int(H/6)),(0, 0, 255),1)
+
+        # moment calculate using middle of the frame
+        if M_mid["m00"] != 0:
+            cX_mid = int(M["m10"] / M["m00"])
+            # cY_top = int(M["m01"] / M["m00"])
+        frame = cv2.circle(frame, (int(cX_mid), int(2*H/3)), 6, (0, 0, 255), -1)
+        frame = cv2.line(frame,(int(cX_mid),0),(int(cX_mid),int(2*H/3)),(0, 0, 255),1)
+
+        frame = cv2.line(frame,(int(cX_top), int(H/6)),(int(cX_mid), int(2*H/3)),(0, 0, 255),2)
+
+        # moment calculate using entire frame
+        # if M["m00"] != 0:
+        #     cX = int(M["m10"] / M["m00"])
+        #     # cY = int(M["m01"] / M["m00"])
+        # frame = cv2.circle(frame, (int(cX), int(H/2)), 5, (0, 0, 255), -1)
+        # frame = cv2.line(frame,(int(cX),0),(int(cX),int(H*2)),(0, 0, 255),2)
 
         # 兩位小數
         self.offset = round( (cX - (W/2))/(W/2)*100 , 2)
