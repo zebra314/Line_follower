3f9f8f81b024dbc3119711f4699721b5ea73e2cb
chrispie181 <superpig181@gmail.com>
2023-03-12 21:42:38 +0800

    Update arduino

--- onArdu/Line_follower/Line_follower.ino
+++ onArdu/Line_follower/Line_follower.ino
@@ -9,144 +9,180 @@
 #define ENCB1  3
 #define ENCA2  18
 #define ENCB2  19
+#define LED  10
 
-int pos_R = 0;
-int pos_L = 0;
-int spd_R = 0;
+long pos_R = 0;
+long pos_L = 0;
+long posd_R = 0;
+long posd_L = 0;
+bool phase1_R = false;
+bool phase1_L = false;
+bool check1 = true;
+unsigned long newtime;
+unsigned long oldtime = 0;
+int START_LAG_L = 0;
+int START_LAG_R = 0;
+int KEEP_LAG_L = 0;
+int KEEP_LAG_R = 0;
+int LAG_L = 0;
+int LAG_R = 0;
+
 int spd_L = 0;
-int V_R = 0;
-int V_L = 0;
-int TRspd_R = 0;
-int TRspd_L = 0;
-String message = "0 0";
+int spd_R = 0;
+
+int TR_R = 2000;
+int TR_L = 2000;
+
+
+String message;
+
+int TRGA;
+int TRGB;
 
 Timer T;
-void working_mode(){
+
+void msg_process(String msg){
   
+  int left, right;
+  
+  TR_L = left;
+  TR_R = right;
 }
+
 void readEncoder1(){
   int b = digitalRead(ENCB1);
   
   if(b>0){
-    pos_R++;
-  }else{
     pos_R--;
+  }else{
+    pos_R++;
   }
 }
 void readEncoder2(){
-  int b = digitalRead(ENCB2);
+  int c = digitalRead(ENCB2);
   
-  if(b>0){
-    pos_L++;
-  }else{
+  if(c>0){
     pos_L--;
+  }else{
+    pos_L++;
   }
 }
 
-void speedReader(){
-  spd_R = pos_R;
-  pos_R = 0;
-  spd_R = pos_R;
-  pos_R = 0;
+void motor_controll(int VO_L, int VO_R){
+  
+  int way_L=0;
+  int way_R=0;
+  int L_V;
+  int R_V;
+  
+  L_V = abs(VO_L);
+  R_V = abs(VO_R);
+  way_L = VO_L/L_V;
+  way_R = VO_R/R_V;
+
+  switch(way_L){//0後退 1左轉 2右轉 3前進 4停止
+    case -1:
+      digitalWrite(L_1, HIGH);
+      digitalWrite(L_2, LOW);
+
+      break;
+    case 0:
+      digitalWrite(L_1, LOW);
+      digitalWrite(L_2, LOW);
+
+      break;
+    case 1:
+      digitalWrite(L_1, LOW);
+      digitalWrite(L_2, HIGH);
+
+      break;
+  }
+  switch(way_R){//0後退 1左轉 2右轉 3前進 4停止
+    case -1:
+      digitalWrite(R_1, HIGH);
+      digitalWrite(R_2, LOW);
+
+      break;
+    case 0:
+      digitalWrite(R_1, LOW);
+      digitalWrite(R_2, LOW);
+
+      break;
+    case 1:
+      digitalWrite(R_1, LOW);
+      digitalWrite(R_2, HIGH);
+
+      break;
+
+  }
+  
+  analogWrite(L_pwm, L_V);
+  analogWrite(R_pwm, R_V);
   
 }
-void motor_turn(){
-  digitalWrite(L_1, LOW);
-  digitalWrite(L_2, HIGH);
-  digitalWrite(R_1, LOW);
-  digitalWrite(R_2, HIGH);
 
-} 
+void controller(){
+  
 
-void motor_speed(int L_spd, int R_spd){
-  analogWrite(L_pwm, L_spd);
-  analogWrite(R_pwm, R_spd);
+  int VO_R = 0;
+  int VO_L = 0;
+  VO_L = TR_L;
+  VO_R = TR_R;
+
+  motor_controll(VO_L, VO_R);
 }
 
-void spd_controll(){
-  //spd_1 , spd_2 , TRspd_1, TRspd_2
-  int error_R;
-  int error_L;
-
-  error_R = TRspd_R - spd_R;
-  error_L = TRspd_L - spd_L;
-
-}
-void display(){
-  Serial.println("---------------------");
-  Serial.print("ACT SPDR ");
-  Serial.print(spd_R);
-  Serial.print(" ==> ");
-  Serial.print("TRA SPDR ");
-  Serial.println(TRspd_R);
-
-  Serial.print("ACT SPDL ");
-  Serial.print(spd_L);
-  Serial.print(" ==> ");
-  Serial.print("TRA SPDL ");
-  Serial.println(TRspd_L);
-
-}
-
-void msg_process(String msg){
-  String a, b;
-  bool flag = false;
-  int left, right;
-  left = 0;
-  right = 0;
-  for(int i = 0;i<msg.length(); i++){
-    if(msg[i] == ' '){
-      flag = true;
-      i++;
-    }
-    if(flag == false){
-      a+=msg[i];
-    }else {
-      b+=msg[i];
-    }
-  }
-  for(int i = a.length()-1, j=0; i>=0; i--, j++){
-    left +=int(a[i]-'0')*pow(10, j);
-  } 
-  for(int i = b.length()-1, j=0; i>=0; i--, j++){
-    right +=int(b[i]-'0')*pow(10, j);
-  }
-  TRspd_L = left;
-  TRspd_R = right;
-
-//  Serial.println(TRspd_L);
-//  Serial.println(TRspd_R);
+void speed(){
+  newtime = millis();
+  spd_R = (pos_R-posd_R) * 1000 /(newtime-oldtime);
+  spd_L = (pos_L-posd_L) * 1000 /(newtime-oldtime);
+  Serial.print ("speed = ");
+  Serial.println (spd_R);
+  posd_R = pos_R;
+  posd_L = pos_L;
+  oldtime = newtime;
 }
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(57600);
+  Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+  
   pinMode(L_1,OUTPUT);
   pinMode(L_2,OUTPUT);
   pinMode(R_1,OUTPUT);
   pinMode(R_2,OUTPUT);
+  
   pinMode(L_pwm,OUTPUT);
   pinMode(R_pwm,OUTPUT);
-  motor_speed(0,0);
+  
+  pinMode(ENCA1,INPUT);
+  pinMode(ENCA2,INPUT);
+  pinMode(ENCB1,INPUT);
+  pinMode(ENCB2,INPUT);
+
   attachInterrupt(digitalPinToInterrupt(ENCA1),readEncoder1,RISING);
   attachInterrupt(digitalPinToInterrupt(ENCA2),readEncoder2,RISING);
-
-  // T.every(100,speedReader);
-  // T.every(1000,display);
+  
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  
 }
 
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (Serial.available() > 0){
-      message = Serial.readString();
-      Serial.println(message);
-  }
-  
-  T.update();
-  motor_turn();
 
-  msg_process(message);
-  motor_speed(TRspd_R,TRspd_L);
+  if (Serial.available() > 0){
+    message = Serial.readString();
+    msg_process(message);
+    Serial.println(message);
+    digitalWrite(LED, HIGH);
+    delay(100);
+  }
+  else{digitalWrite(LED, LOW);}
+
+  T.update();
+  speed();
   
+  controller();
 }
